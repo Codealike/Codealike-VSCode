c65d7423437fdf5c53e77b9d8c1e1a24d6e79c34
Daniel Iglesias <diglesias17@hotmail.com>
2018-02-12 12:40:38 -0300

    Version updated to 0.0.20

--- extension.js
+++ extension.js
@@ -16,7 +16,7 @@ function activate(context) {
         statusBarItem.text = "Codealike is initializing...";
 
         // initialize plugin for current client and version
-        Codealike.initialize('vscode', '0.0.19');
+        Codealike.initialize('vscode', '0.0.20');
 
         Codealike.registerStateSubscriber((state) => {
             if (state.isTracking) {
